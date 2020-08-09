ce738a298c080a02b21898697ec424de2f8c0c5a
ZeroCool-code <62298173+ZeroCool-code@users.noreply.github.com>
2020-08-09 12:41:22 +0200

    Update calc.js

--- calc.js
+++ calc.js
@@ -7,7 +7,7 @@ function get (){
 var b = document.getElementsByClassName("numbers").length;
 for(var i = 0 ; i < b ; i++) {
 document.querySelectorAll('button.numbers')[i].addEventListener("click", function(){
-   var sound1 = new Audio("sound/click.mp3");
+   var sound1 = new Audio("click.mp3");
    sound1.play();
    var f = this.value;
    document.getElementById("result").value +=f;
@@ -21,7 +21,7 @@ document.getElementById("calc").addEventListener("click" , function(){
    var screenText = document.getElementById("result").innerHTML;
    var yy = eval(screenValue);
    document.getElementById("result").value = yy;
-   var sound2 = new Audio("sound/calcu.mp3");
+   var sound2 = new Audio("calcu.mp3");
         sound2.play();
    
   
